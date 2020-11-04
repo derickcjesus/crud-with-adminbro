05939d151b4a9c8290882d1de22a491e4ca26d8e
Derick de Jesus <derickcjesus@gmail.com>
2020-11-03 21:29:58 -0300

    config db

--- admin.js
+++ admin.js
@@ -1,18 +1,48 @@
+// Database
+const mongoose = require("mongoose");
+const { Script } = require("vm");
+
+const ProjectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: Script,
+  completed: Boolean,
+  created_at: { type: Date, default: Date.now },
+});
+
+const Project = mongoose.model("Project", ProjectSchema);
+
 // Admin Bro
-const AdminBro = require("admin-bro")
-const AdminBroExpress = require("@admin-bro/express")
+const AdminBro = require("admin-bro");
+const AdminBroExpress = require("@admin-bro/express");
+const AdminBroMongoose = require("@admin-bro/mongoose");
 
-const adminBro = new AdminBro({
-    databases: [],
-    rootPath: "/admin",
-})
+// use mongoose in adminbro
+AdminBro.registerAdapter(AdminBroMongoose);
 
-const router = AdminBroExpress.buildRouter(adminBro)
+// config
+const adminBroOptions = new AdminBro({
+  resources: [Project],
+  rootPath: "/admin",
+});
+const router = AdminBroExpress.buildRouter(adminBroOptions);
 
 // Server
 const express = require("express");
 const server = express();
 
-server
-    .use(adminBro.options.rootPath, router)
-    .listen(5500, () => console.log("Server started"));
+server.use(adminBroOptions.options.rootPath, router);
+
+// Run app
+const run = async () => {
+  await mongoose.connect("mongodb://localhost/adminbroapp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  await server.listen(5500, () => console.log("Server started"));
+};
+
+run();
